2035274f6ea5e5997f84f39afd3fc773b52aa2d1
Salome Thirot <salome.thirot@arm.com>
2024-10-22 15:18:11 +0100

    arm: opts part 6 - SSE & full_distortion_kernel
    Move the DotProd impl. of svt_aom_sse to its own file
    
    svt_aom_sse_neon had some DotProd helpers hidden behind flags, move them
    to a separate neon_dotprod file and create a proper dot-product
    implementation of this function.
    
    Move svt_aom_highbd_sse_neon to a separate file
    
    Move svt_aom_highbd_sse_neon to its own high bitdepth file.
    
    Port libaom Neon implementation of svt_aom_highbd_sse
    
    Replace the current implementation of svt_aom_highbd_sse_neon with the
    libaom version, which is up to 3 times faster.
    
    Add SVE implementation of svt_aom_highbd_sse
    
    Port the libaom SVE implementation of svt_aom_highbd_sse and add the
    corresponding tests.
    
    Fix input size for svt_full_distortion_kernel16_bits unit tests
    
    Change the input size from 15 bits to 10 bits, as it is the maximum
    bitdepth supported by SVT-AV1.
    
    Use highbd sse helper for svt_full_distortion_kernel16_bits_neon
    
    Reuse the highbd_sse helper functions for
    svt_full_distortion_kernel16_bits_neon, which gives up to a 70%
    reduction in cycle count.
    
    Add DotProd implementation of svt_spatial_full_kernel_distortion
    
    Reuse the DotProd SSE helper functions to write a DotProd implementation
    of svt_spatial_full_kernel_distortion and add the corresponding unit
    tests.
    
    Add SVE implementation of svt_full_distortion_kernel16_bits
    
    Reuse the SVE SSE helper functions to write an SVE implementation of
    svt_full_distortion_kernel16_bits and add the corresponding unit tests.
    
    Optimize svt_full_distortion_kernel32_bits_neon
    
    Small optimization for svt_full_distortion_kernel32_bits_neon, giving up
    to 5% reduction in cycle count.

--- test/CompoundUtilTest.cc
+++ test/CompoundUtilTest.cc
@@ -1161,6 +1161,13 @@ INSTANTIATE_TEST_SUITE_P(
     NEON, AomSseTest,
     ::testing::Combine(::testing::Range(BLOCK_4X4, BlockSizeS_ALL),
                        ::testing::Values(svt_aom_sse_neon)));
+
+#if HAVE_NEON_DOTPROD
+INSTANTIATE_TEST_SUITE_P(
+    NEON_DOTPROD, AomSseTest,
+    ::testing::Combine(::testing::Range(BLOCK_4X4, BlockSizeS_ALL),
+                       ::testing::Values(svt_aom_sse_neon_dotprod)));
+#endif  // HAVE_NEON_DOTPROD
 #endif  // ARCH_AARCH64
 
 class AomSseHighbdTest : public ::testing::TestWithParam<AomSseParam> {
@@ -1240,6 +1247,13 @@ INSTANTIATE_TEST_SUITE_P(
     ::testing::Combine(::testing::Range(BLOCK_4X4,
                                         (BlockSize)(BlockSizeS_ALL + 2)),
                        ::testing::Values(svt_aom_highbd_sse_neon)));
+#if HAVE_SVE
+INSTANTIATE_TEST_SUITE_P(
+    SVE, AomSseHighbdTest,
+    ::testing::Combine(::testing::Range(BLOCK_4X4,
+                                        (BlockSize)(BlockSizeS_ALL + 2)),
+                       ::testing::Values(svt_aom_highbd_sse_sve)));
+#endif  // HAVE_SVE
 #endif  // ARCH_AARCH64
 
 typedef void (*AomSubtractBlockFunc)(int, int, int16_t *, ptrdiff_t,
